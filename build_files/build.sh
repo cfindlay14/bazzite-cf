d1dc25cf706a8a7bd55c9d78895d1d1c7fc12f91
cfindlay14 <cfindlay14@protonmail.com>
2026-01-06 15:30:38 +0000

    Remove Bitwarden desktop installation command

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,7 +19,7 @@ dnf5 install -y @virtualization
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
-system-flatpak install flathub com.bitwarden.desktop
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
